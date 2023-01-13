b36f53a19fb496ab26e128d85f9bd679067608d9
khaled <khaledabdel.dev@gmail.com>
2023-01-13 02:35:44 -0500

    REinitialization

--- public/main.js
+++ public/main.js
@@ -1,25 +1,25 @@
+document.querySelector('#button').addEventListener('click', getTimes)
 
-const deleteButton = document.querySelector('#delete-button')
-
-
-deleteButton.addEventListener('click', _ => {
-  console.log('working')
-    fetch('/times', {
-      method: 'delete',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        type: 'CREDIT',
-        type: 'DEBIT'
-      })
-    })
-      .then(res => {
-        if (res.ok) return res.json()
-    })
-    .then(response => {
-        if (response === 'No entry to delete') {
-          console.log('Transactions clear')
-        } else {
-          window.location.reload(true)
-        }
-      })
-})
+function getTimes(){
+    let URL = `https://ipgeolocation.abstractapi.com/v1/?api_key=bd0c0ed148014f0ea09ab35dd2b55b0c`
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.ip_address)
+        let IP = data.ip_address
+        fetch(`http://www.islamicfinder.us/index.php/api/prayer_times/?user_ip=${IP}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            document.querySelector('.location').innerText = 'Prayer in ' + data.settings.location.city + ', ' + data.settings.location.country
+            document.querySelector('.fajr').innerText = data.results.Fajr.substr(0,5) + ' AM'
+            document.querySelector('.duhr').innerText = data.results.Dhuhr.substr(0,5) + ' PM'
+            document.querySelector('.asr').innerText = data.results.Asr.substr(0,5) + ' PM'
+            document.querySelector('.maghrib').innerText = data.results.Maghrib.substr(0,5) + ' PM'
+            document.querySelector('.isha').innerText = data.results.Isha.substr(0,5) + ' PM'
+        })
+    .catch(err => {
+        console.log(`error ${err}`)
+        
+    });
+})}
--- server.js
+++ server.js
@@ -5,25 +5,26 @@ const MongoClient = require('mongodb').MongoClient
 
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+// app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 
 
-MongoClient.connect('mongodb+srv://khaled:Jamilo78@cluster0.77z2sbo.mongodb.net/test', { useUnifiedTopology: true })
+MongoClient.connect('mongodb+srv://demo:demo@cluster0.ts4r6fz.mongodb.net/test', { useUnifiedTopology: true })
     .then(client => {
-            console.log('Connected to Database')
-            const db = client.db('CRUD-demo')
-            const timesCollection = db.collection('money')
+            console.log('Connected to SalahTime')
+            const db = client.db('salaah')
+            const salahCollection = db.collection('salah')
             app.use(bodyParser.urlencoded({ extended: true }))
             app.get('/', (req, res) => {
-                db.collection('money').find().toArray()
+                db.collection('salah').find().toArray()
                 .then(results => {
                     res.render('index.ejs', { times: results })
                 })
                 .catch(error => console.error(error))
             })
             app.post('/times', (req, res) => {
-                timesCollection.insertOne(req.body)
+                salahCollection.insertOne(req.body)
                 .then(result => {
                     res.redirect('/')
                 })
@@ -32,7 +33,7 @@ MongoClient.connect('mongodb+srv://khaled:Jamilo78@cluster0.77z2sbo.mongodb.net/
             app.listen(3000, function() {
                 console.log('listening on 3000')
             })
-            app.put('/times', (req, res) => {
+            /*app.put('/times', (req, res) => {
                 console.log(req.body)
                 timesCollection.findOneAndUpdate(
                     { name: req.body.name,
@@ -61,7 +62,7 @@ MongoClient.connect('mongodb+srv://khaled:Jamilo78@cluster0.77z2sbo.mongodb.net/
                     res.json(`Deleted entry`)
                   })
                   .catch(error => console.error(error))
-            })
+            })*/
         })
     .catch(error => console.log(error)) 
 
